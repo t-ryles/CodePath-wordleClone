33e1b5b32f3a4874e2c11713047519c5a2668a72
t-ryles <tajhakeem.ryles@gmail.com>
2022-09-14 15:35:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/codepath_wordleclone/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.codepath_wordleclone
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val wordToGuess = findViewById<TextView>(R.id.word)
+
+        fun checkGuess(guess: String) : String {
+            var result = ""
+            for (i in 0..3) {
+
+                if (guess[i] == wordToGuess[i]) {
+                    result += "O"
+                }
+                else if (guess[i] in wordToGuess) {
+                    result += "+"
+                }
+                else {
+                    result += "X"
+                }
+            }
+            return result
+        }
+
+    }
+
+}
