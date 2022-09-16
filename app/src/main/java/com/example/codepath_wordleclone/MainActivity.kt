36fa2b6a0c87cb5e6b35b409bb09617c3056959a
t-ryles <tajhakeem.ryles@gmail.com>
2022-09-15 22:07:08 -0500

    -UI- Updated UI with user input Updated UI with output on user guess Removed button after 3 guesses Displayed random word after 3 guesses
    -Code-
    Made counter for user guesses

--- app/src/main/java/com/example/codepath_wordleclone/MainActivity.kt
+++ app/src/main/java/com/example/codepath_wordleclone/MainActivity.kt
@@ -1,33 +1,88 @@
 package com.example.codepath_wordleclone
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Getting view ID's
         val wordToGuess = findViewById<TextView>(R.id.word)
 
-        fun checkGuess(guess: String) : String {
+        val button:Button = findViewById(R.id.guessBTN)
+
+        val firstInput = findViewById<TextView>(R.id.firstGuessInput)
+        val secondInput = findViewById<TextView>(R.id.secondGuessInput)
+        val thirdInput = findViewById<TextView>(R.id.thirdGuessInput)
+        val guessedWord:EditText = findViewById(R.id.guessInput)
+
+        val firstOutput = findViewById<TextView>(R.id.firstGuessOutput)
+        val secondOutput = findViewById<TextView>(R.id.secondGuessOutput)
+        val thirdOutput = findViewById<TextView>(R.id.thirdGuessOutput)
+
+
+        //Setting up guess count
+        var guessCount = 0
+
+        //Getting random word
+        val randomWord = FourLetterWordList.getRandomFourLetterWord()
+        //Displaying randomWord in word textView
+        randomWord.also { wordToGuess.text = it }
+
+        /**
+         * Parameters / Fields:
+         *   wordToGuess : String - the target word the user is trying to guess
+         *   guessedWord : String - what the user entered as their guess
+         *
+         * Returns a String of 'O', '+', and 'X', where:
+         *   'O' represents the right letter in the right place
+         *   '+' represents the right letter in the wrong place
+         *   'X' represents a letter not in the target word
+         */
+        fun checkGuess(guessedWord: String) {
             var result = ""
             for (i in 0..3) {
-
-                if (guess[i] == wordToGuess[i]) {
+                if (guessedWord[i] == randomWord[i]) {
                     result += "O"
                 }
-                else if (guess[i] in wordToGuess) {
+                else if (guessedWord[i] in randomWord) {
                     result += "+"
                 }
                 else {
                     result += "X"
                 }
             }
-            return result
+            when(guessCount) {
+                1 -> firstOutput.text = result
+                2 -> secondOutput.text = result
+                3 -> thirdOutput.text = result
+            }
         }
 
-    }
 
+        //Getting the user input on click
+            button.setOnClickListener {
+                guessCount++
+
+                checkGuess(guessedWord.toString())
+
+                //Added user input to input textViews
+                when(guessCount) {
+                    1 -> firstInput.text = guessedWord.text
+                    2 -> secondInput.text = guessedWord.text
+                    3 -> thirdInput.text = guessedWord.text
+                }
+                if(guessCount == 3 ){
+                    button.visibility = View.INVISIBLE
+                    wordToGuess.visibility = View.VISIBLE
+                }
+            }
+        }
 }
