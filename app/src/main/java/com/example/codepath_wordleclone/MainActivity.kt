ad5342a9062f901c475c94cea5b729456e5821cc
t-ryles <tajhakeem.ryles@gmail.com>
2022-09-16 20:31:01 -0500

    -UI- Reset button to clear all views Updated UI for guessedWord and randomWord comparison.
    -Code-
    Added reset button
    Compared guessedWord to randomWord

--- app/src/main/java/com/example/codepath_wordleclone/MainActivity.kt
+++ app/src/main/java/com/example/codepath_wordleclone/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         val wordToGuess = findViewById<TextView>(R.id.word)
 
         val button:Button = findViewById(R.id.guessBTN)
+        val reset:Button = findViewById(R.id.resetBTN)
 
         val firstInput = findViewById<TextView>(R.id.firstGuessInput)
         val secondInput = findViewById<TextView>(R.id.secondGuessInput)
@@ -32,27 +34,33 @@ class MainActivity : AppCompatActivity() {
         var guessCount = 0
 
         //Getting random word
-        val randomWord = FourLetterWordList.getRandomFourLetterWord()
+        var randomWord = FourLetterWordList.getRandomFourLetterWord()
+
         //Displaying randomWord in word textView
         randomWord.also { wordToGuess.text = it }
 
+
         /**
          * Parameters / Fields:
-         *   wordToGuess : String - the target word the user is trying to guess
-         *   guessedWord : String - what the user entered as their guess
+         *   randomWord : String - the target word the user is trying to guess
+         *   guess : String - what the user entered as their guess
          *
          * Returns a String of 'O', '+', and 'X', where:
          *   'O' represents the right letter in the right place
          *   '+' represents the right letter in the wrong place
          *   'X' represents a letter not in the target word
          */
-        fun checkGuess(guessedWord: String) {
+        fun checkGuess(guess: String) {
+
             var result = ""
+
             for (i in 0..3) {
-                if (guessedWord[i] == randomWord[i]) {
+                if (guess[i] == randomWord[i]) {
+
                     result += "O"
                 }
-                else if (guessedWord[i] in randomWord) {
+                else if (guess[i] in randomWord) {
+
                     result += "+"
                 }
                 else {
@@ -71,7 +79,7 @@ class MainActivity : AppCompatActivity() {
             button.setOnClickListener {
                 guessCount++
 
-                checkGuess(guessedWord.toString())
+                checkGuess(guessedWord.text.toString().uppercase())
 
                 //Added user input to input textViews
                 when(guessCount) {
@@ -79,10 +87,34 @@ class MainActivity : AppCompatActivity() {
                     2 -> secondInput.text = guessedWord.text
                     3 -> thirdInput.text = guessedWord.text
                 }
-                if(guessCount == 3 ){
+                if((guessCount == 3) || (guessedWord.text.toString().uppercase() == randomWord)){
                     button.visibility = View.INVISIBLE
+                    reset.visibility = View.VISIBLE
                     wordToGuess.visibility = View.VISIBLE
                 }
+
+                guessedWord.text.clear()
+
+            }
+
+        //Reset game
+            reset.setOnClickListener {
+                0.also { guessCount = it }
+
+                firstInput.text = ""
+                secondInput.text = ""
+                thirdInput.text = ""
+
+                firstOutput.text = ""
+                secondOutput.text = ""
+                thirdOutput.text = ""
+
+                button.visibility = View.VISIBLE
+                reset.visibility = View.INVISIBLE
+                wordToGuess.visibility = View.INVISIBLE
+
+                randomWord = FourLetterWordList.getRandomFourLetterWord()
+
             }
         }
 }
